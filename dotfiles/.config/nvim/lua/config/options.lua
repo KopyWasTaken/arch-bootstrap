b21910e20806c0cc493152cc14fb9601d863815b
Kyler Kopacz <kkopacz@chewy.com>
2026-01-30 11:53:31 -0500

    fixed some options

--- dotfiles/.config/nvim/lua/config/options.lua
+++ dotfiles/.config/nvim/lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Turn off autoformatting
+vim.g.autoformat = false
+
+-- I don't want animations
+vim.g.snacks_animate = false
+
+-- Options
+local opt = vim.opt
+
+opt.shiftwidth = 4 -- size of indentations
+opt.tabstop = 4 -- number of spaces tabs count for

28c5f7af443da81c61432d727a59de3053de49cd
Kyler Kopacz <kkopacz@chewy.com>
2026-02-02 13:28:41 -0500

    updating configs

--- dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,5 @@
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
-    -- disable the default keybinging
-    { "<leader>/", false },
-    -- change it to something else
-    { "<leader>fw", "<cmd>Telescope grep_string<cr>", desc = "Find [W]ord in files" },
   },
 }
